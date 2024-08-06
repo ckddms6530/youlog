738b419c835514cdfcfc217c5cde81c13a2fc7ec
유창은 <ckddms6530@gmail.com>
2024-08-06 15:43:12 +0900

    [add] : JPA Auditing 기능 사용을 위한 Config 클래스 추가

---
+++ src/main/java/com/youlog/youlog/common/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.youlog.youlog.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
