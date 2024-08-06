b9ee21d5d475346bbd1b837ee0ace733937a5f03
유창은 <ckddms6530@gmail.com>
2024-08-06 15:42:39 +0900

    [add] : JPA 매핑 시 공통 속성을 위한 BaseEntity 추가

---
+++ src/main/java/com/youlog/youlog/common/model/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.youlog.youlog.common.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
