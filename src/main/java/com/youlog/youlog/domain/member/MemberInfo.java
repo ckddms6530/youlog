f1d3e5da1e62d3f4fd66e99f68c27e8988a803da
유창은 <ckddms6530@gmail.com>
2024-08-06 15:59:04 +0900

    [add] : 기존 Member 클래스 삭제 후 MemberInfo 클래스 추가

---
+++ src/main/java/com/youlog/youlog/domain/member/MemberInfo.java
@@ -0,0 +1,32 @@
+package com.youlog.youlog.domain.member;
+
+import com.youlog.youlog.common.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "member_info")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberInfo extends BaseEntity {
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "latest_login_at")
+    private LocalDateTime latestLoginAt;
+
+    protected MemberInfo(String nickname){
+        this.nickname = nickname;
+    }
+    public void updateNickname(String nickname){
+        this.nickname = nickname;
+    }
+
+}
