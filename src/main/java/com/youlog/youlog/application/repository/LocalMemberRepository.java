cb8df02370cea5e6a660bd32178b9cb75367c4d9
유창은 <ckddms6530@gmail.com>
2024-08-07 21:06:21 +0900

    [feat] : 이메일로 직접가입 회원 조회를 위한  findByEmail 메서드 추가

--- src/main/java/com/youlog/youlog/application/repository/LocalMemberRepository.java
+++ src/main/java/com/youlog/youlog/application/repository/LocalMemberRepository.java
@@ -3,5 +3,9 @@ package com.youlog.youlog.application.repository;
 import com.youlog.youlog.domain.member.LocalMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LocalMemberRepository extends JpaRepository<LocalMember, Long> {
+
+    Optional<LocalMember> findByEmail(String email);
 }
