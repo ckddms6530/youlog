a8f8f2cf3c03e295b922013de595b9bbf986ba3d
유창은 <ckddms6530@gmail.com>
2024-08-07 13:17:19 +0900

    [feat] : Category JPA 엔티티 매핑

--- src/main/java/com/youlog/youlog/domain/category/Category.java
+++ src/main/java/com/youlog/youlog/domain/category/Category.java
@@ -1,15 +1,27 @@
 package com.youlog.youlog.domain.category;
 
+import com.youlog.youlog.common.model.BaseEntity;
+import com.youlog.youlog.domain.blog.Blog;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "category")
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Category {
+public class Category extends BaseEntity {
 
-    private Long id;
+    @Column
     private String name;
-    private Long parentId;
-    private Long blogId;
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+    @ManyToOne
+    @JoinColumn(name = "blog_id")
+    private Blog blog;
+    @Embedded
     private CategoryHierarchy categoryHierarchy;
 
     public void updateHierarchy(CategoryHierarchy categoryHierarchy) {
@@ -20,13 +32,15 @@ public class Category {
         this.name = name;
     }
 
-    public static Category createCategory(Long blogId, String name, Long parentId, CategoryHierarchy categoryHierarchy) {
-        return new Category(blogId, name, parentId, categoryHierarchy);
-    }
 
+    @Embeddable
+    @NoArgsConstructor
     public static class CategoryHierarchy {
+        @Column(name = "ref")
         private Integer ref;
+        @Column(name = "level")
         private Integer level;
+        @Column(name = "seq")
         private Integer seq;
 
         private CategoryHierarchy(Integer ref, Integer level, Integer seq) {
@@ -43,10 +57,10 @@ public class Category {
         }
     }
 
-    private Category(Long blogId, String name, Long parentId, CategoryHierarchy categoryHierarchy) {
-        this.blogId = blogId;
+    public Category(Blog blog, String name, Category parentCategory, CategoryHierarchy categoryHierarchy) {
+        this.blog = blog;
         this.name = name;
-        this.parentId = parentId;
+        this.parent = parentCategory;
         this.categoryHierarchy = categoryHierarchy;
     }
 
