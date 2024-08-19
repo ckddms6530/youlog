e1c4dd2d9835e9fdab2530fc8512f56b211da6be
유창은 <ckddms6530@gmail.com>
2024-08-19 14:11:44 +0900

    [feat] : model에 공통적으로 url 정보를 담기 위한 controlleradvice

--- src/main/java/com/youlog/youlog/common/advice/ControllerAdvices.java
+++ src/main/java/com/youlog/youlog/common/advice/ControllerAdvices.java
@@ -1,17 +1,28 @@
 package com.youlog.youlog.common.advice;
 
+import com.youlog.youlog.common.properties.UrlProperties;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
+@RequiredArgsConstructor
 @Slf4j
 public class ControllerAdvices {
 
     private static final String REDIRECT_PREFIX = "redirect:";
+    private final UrlProperties urlProperties;
+
+    @ModelAttribute
+    public void addUrls(Model model) {
+        model.addAttribute("urls", urlProperties);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public String methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e, HttpServletRequest request, RedirectAttributes redirectAttributes) {
@@ -19,4 +30,6 @@ public class ControllerAdvices {
         redirectAttributes.addFlashAttribute("error", e.getAllErrors().get(0).getDefaultMessage());
         return REDIRECT_PREFIX + request.getRequestURI();
     }
+
+
 }
