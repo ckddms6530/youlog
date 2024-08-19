5f1200dd58937b9be6639f44b76c8097ddb70ab2
유창은 <ckddms6530@gmail.com>
2024-08-19 12:44:58 +0900

    [feat] : 관리자 Id로 블로그 조회하기 위한 메서드 추가

--- src/main/java/com/youlog/youlog/application/repository/BlogRepository.java
+++ src/main/java/com/youlog/youlog/application/repository/BlogRepository.java
@@ -3,5 +3,8 @@ package com.youlog.youlog.application.repository;
 import com.youlog.youlog.domain.blog.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BlogRepository extends JpaRepository<Blog, Long> {
+    Optional<Blog> findByAdmin_Id(Long id);
 }
