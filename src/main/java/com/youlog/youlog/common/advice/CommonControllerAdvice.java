bc51932d46bde61f213edb60fb9883ea4a73b81b
유창은 <ckddms6530@gmail.com>
2024-08-26 22:44:49 +0900

    [feat] : ErrorCode 관리를 위한 Enum 추가 및 BusinessException 에러 핸들링을 위한 메서드 추가

--- src/main/java/com/youlog/youlog/common/advice/CommonControllerAdvice.java
+++ src/main/java/com/youlog/youlog/common/advice/CommonControllerAdvice.java
@@ -1,9 +1,11 @@
 package com.youlog.youlog.common.advice;
 
+import com.youlog.youlog.common.error.BusinessException;
 import com.youlog.youlog.common.properties.UrlProperties;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,13 +13,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Locale;
+
 @ControllerAdvice
 @RequiredArgsConstructor
 @Slf4j
-public class ControllerAdvices {
+public class CommonControllerAdvice {
 
     private static final String REDIRECT_PREFIX = "redirect:";
     private final UrlProperties urlProperties;
+    private final MessageSource messageSource;
 
     @ModelAttribute
     public void addUrls(Model model) {
@@ -31,5 +36,10 @@ public class ControllerAdvices {
         return REDIRECT_PREFIX + request.getRequestURI();
     }
 
-
+    @ExceptionHandler(BusinessException.class)
+    public String handleBusinessException(BusinessException e, HttpServletRequest request, RedirectAttributes redirectAttributes, Locale locale) {
+        log.debug("BusinessException: {}", e.getMessage());
+        redirectAttributes.addFlashAttribute("error", messageSource.getMessage(e.getErrorCode().getCode(), null, locale));
+        return REDIRECT_PREFIX + request.getHeader("Referer");
+    }
 }
--- src/main/java/com/youlog/youlog/common/error/ErrorCode.java
+++ src/main/java/com/youlog/youlog/common/error/ErrorCode.java
@@ -6,9 +6,12 @@ import lombok.Getter;
 public enum ErrorCode {
 
     BLOG_CREATION_LIMIT(400, "BLOG-001", "블로그 생성 제한을 초과"),
+    BLOG_NOT_FOUND(404, "BLOG-002", "블로그를 찾을 수 없음"),
+    MEMBER_NOT_FOUND(404, "MEMBER-001", "회원를 찾을 수 없음"),
+    MEMBER_DUPLICATION(400, "MEMBER-002", "이미 존재하는 회원"),
     INVALID_INPUT_VALUE(400, "COMMON-001", "입력 값이 잘못된 경우"),
     INTERNAL_SERVER_ERROR(500, "COMMON-002", "내부 서버 오류"),
-    ACCESS_DENIED(403, "COMMON-003", "접근 권한이 없는 경우");
+    ACCESS_DENIED(403, "COMMON-003", "접근 권한이 없는 경우"), CATEGORY_NOT_FOUND(404, "CATE-001", "카테고리를 찾을 수 없음");
 
 
     private final int status;
