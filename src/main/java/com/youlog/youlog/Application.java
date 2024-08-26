db990566639a520aeb3421a7feba8e006e637367
유창은 <ckddms6530@gmail.com>
2024-08-26 22:45:39 +0900

    [refactor] : properties scan 경로 하드코딩에서 마커인터페이스 지정

--- src/main/java/com/youlog/youlog/Application.java
+++ src/main/java/com/youlog/youlog/Application.java
@@ -1,11 +1,12 @@
 package com.youlog.youlog;
 
+import com.youlog.youlog.common.properties.PropertiesMaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan("com.youlog.youlog.common.properties")
+@ConfigurationPropertiesScan(basePackageClasses = PropertiesMaker.class)
 public class Application {
 
     public static void main(String[] args) {
