40a07430f53676033a148d2173cff1b66cfcfb8f
유창은 <ckddms6530@gmail.com>
2024-08-26 22:46:50 +0900

    [feat] : 이미 존재 하는 회원이 가입 시 예외처리

--- src/main/java/com/youlog/youlog/application/MemberService.java
+++ src/main/java/com/youlog/youlog/application/MemberService.java
@@ -1,8 +1,10 @@
 package com.youlog.youlog.application;
 
-import com.youlog.youlog.application.repository.LocalMemberRepository;
+import com.youlog.youlog.common.error.BusinessException;
+import com.youlog.youlog.common.error.ErrorCode;
 import com.youlog.youlog.domain.member.LocalMember;
 import com.youlog.youlog.domain.member.MemberInfo;
+import com.youlog.youlog.infrastructure.repository.member.LocalMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,9 @@ public class MemberService {
     private final PasswordEncoder passwordEncoder;
 
     public void registerMember(String email, String password, String nickname) {
+        localMemberRepository.findByEmail(email).ifPresent(localMember -> {
+            throw new BusinessException(ErrorCode.MEMBER_DUPLICATION);
+        });
         MemberInfo memberInfo = new MemberInfo(nickname);
         String hashedPassword = passwordEncoder.encode(password);
         LocalMember localMember = LocalMember.builder()
@@ -26,4 +31,5 @@ public class MemberService {
                 .build();
         localMemberRepository.save(localMember);
     }
+
 }
